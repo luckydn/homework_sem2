77c6c8a3fe3d6cd3af3a3007c584868c0e9f440d
luckydn <sp.donbass@gmail.com>
2022-07-04 01:52:19 +0300

    выполнил 13 задачу

---
+++ hw13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число и программа определит третью цифру заданного числа");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 100)
+    {
+        int result = number % 10;
+        Console.WriteLine($"{number} --> {result}");
+    }
+else
+    {
+        Console.WriteLine("У заданного числа нет третьей цифры");
+    }
