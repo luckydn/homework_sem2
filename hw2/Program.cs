12db722b71812c44ea2a8c76eda84cba94994428
luckydn <sp.donbass@gmail.com>
2022-07-03 23:59:51 +0300

    выполнил 3 задачу

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,8 +1,10 @@
 ﻿Console.WriteLine("Введите два числа для проверки, является ли второе число кратным первому.");
+
 int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if(numberA % numberB <= 0)
+
+if(numberA % numberB == 0)
 {
     Console.WriteLine($"Число кратно и делится без остатка");
 }
