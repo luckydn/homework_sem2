d7bd242e3c810b15a2ce72da15634deea77fd9f9
luckydn <sp.donbass@gmail.com>
2022-07-04 01:42:42 +0300

    выполнил задачу 10

---
+++ hw10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите трехзначное число, что бы программа определила 2е число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99)
+    {
+        if(number < 1000)
+            {
+                int result = (number % 100) / 10;
+                Console.WriteLine($"{number} -> {result}");
+            }
+        else
+            {
+                Console.WriteLine("Число не являлется трехзначным. Попробуйте снова.");
+            }
+    }
+else
+    {
+        Console.WriteLine("Число не являлется трехзначным. Попробуйте снова.");
+
+    }
+
+
+
