1fa30c7d8f553ce6453b1f920d0569f1f2922900
luckydn <sp.donbass@gmail.com>
2022-07-03 22:56:10 +0300

    выполнил задачу 1

---
+++ hw1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное 
+//трёхзначное число и 
+//удаляет вторую цифру этого числа
+
+
+
+int randomNumber = new Random().Next(100, 999);
+int result = randomNumber;
+int number1 = result / 100;
+int number2 = (result % 100) / 10;
+int number3 = result % 10;
+Console.WriteLine($"Случайное число {result} --> {number1}{number3}");
