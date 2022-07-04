b2a7db1fda47ef61c92c025881507d2c3da47aeb
luckydn <sp.donbass@gmail.com>
2022-07-04 03:01:17 +0300

    выполнил 15 задачу

---
+++ hw15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите цифру от 1-7, что бы программа определила день недели, и проверила является ли этот день выходным");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] day = {"Понедельник", "Вторник", "Среда", "Четвер", "Пятница", "Суббота", "Воскресенье" };
+int i = number - 1;
+if(number < 7)
+    {
+        if(number == 5)
+            {
+                string result = day[i];
+                Console.WriteLine($"{result} Выходной день");
+            }
+        else
+            {
+                string result = day[i];
+                Console.WriteLine($"{result} Рабочий день");
+            }
+    }
+else
+    {
+       Console.WriteLine("Не верное значение, укажите число от 1 до 7");
+    }
