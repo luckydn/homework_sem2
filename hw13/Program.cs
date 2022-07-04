f55d586408d8c42ffd6b12888c5cee7a001d53e0
luckydn <107429613+luckydn@users.noreply.github.com>
2022-07-04 15:41:52 +0300

    Update Program.cs

--- hw13/Program.cs
+++ hw13/Program.cs
@@ -1,9 +1,24 @@
-﻿Console.WriteLine("Введите число и программа определит третью цифру заданного числа");
+Console.WriteLine("Введите число и программа определит третью цифру заданного числа");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number > 100)
+if(number > 100 && number < 1000)
     {
         int result = number % 10;
+        Console.WriteLine($"{number} --> {result}");
+    }
+else if(number > 1000 && number < 10000)
+    {
+        int result = (number % 100) / 10;
+        Console.WriteLine($"{number} --> {result}");
+    }
+else if(number > 10000 && number < 100000)
+    {
+        int result = (number % 1000) / 100;
+        Console.WriteLine($"{number} --> {result}");
+    }
+else if(number > 100000 && number < 1000000)
+    {
+        int result = (number % 10000) / 1000;
         Console.WriteLine($"{number} --> {result}");
     }
 else
